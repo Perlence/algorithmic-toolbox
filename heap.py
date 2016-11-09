e99b151c4e864d8d2a61d556fbe5410a4794d4e1
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2016-11-09 22:47:03 +0300

    Solve 'Priority queue' using custom max-heap

--- heap.py
+++ heap.py
@@ -13,7 +13,6 @@ def main():
 
 def cli(it, w):
     count = int(next(it))
-    # heap = BuiltinMaxHeap()
     heap = MaxHeap()
     for line in islice(it, count):
         words = line.split()
@@ -91,44 +90,57 @@ class MaxHeap:
 
     def _sift_up(self, i):
         while True:
-            parent_id = self._parent_of(i)
+            parent_id = _parent_of(i)
             if parent_id < 0 or self._list[parent_id] >= self._list[i]:
                 break
             self._list[parent_id], self._list[i] = self._list[i], self._list[parent_id]
             i = parent_id
 
-    @profile
     def _sift_down(self, i):
-        l = self._list
+        ls = self._list
+        ll = len(ls)
         while True:
-            child_ids = self._children_of(i)
-            nhood = [(i, l[i])] + [(child_id, l[child_id]) for child_id in child_ids if child_id < len(l)]
-            max_id, max_value = max(nhood, key=lambda t: t[1])
-            if max_id == i:
+            l, r = _children_of(i)
+            if r < ll and ls[i] < ls[r] >= ls[l]:
+                max_id = r
+            elif l < ll and ls[i] < ls[l]:
+                max_id = l
+            else:
                 break
-            l[max_id], l[i] = l[i], l[max_id]
+            ls[max_id], ls[i] = ls[i], ls[max_id]
             i = max_id
-
-    def _parent_of(self, i):
-        return math.ceil(i / 2) - 1
-
-    def _children_of(self, i):
-        first = 2*i + 1
-        return (first, first + 1)
 
     def __repr__(self):
         return 'MaxHeap({!r})'.format(self._list)
 
 
-def test_extract_max():
-    h = MaxHeap([66, 57, 42, 11, 52, 29, 29])
-    assert h.extract_max() == 66
-    assert h.extract_max() == 57
-    assert h.extract_max() == 52
-    assert h.extract_max() == 42
-    assert h.extract_max() == 29
-    assert h.extract_max() == 29
-    assert h.extract_max() == 11
+def _parent_of(i):
+    return math.ceil(i / 2) - 1
+
+
+def _children_of(i):
+    first = 2*i + 1
+    return (first, first + 1)
+
+
+def test_children_of():
+    assert _children_of(0) == (1, 2)
+    assert _children_of(1) == (3, 4)
+    assert _children_of(2) == (5, 6)
+    assert _children_of(3) == (7, 8)
+    assert _children_of(4) == (9, 10)
+    assert _children_of(5) == (11, 12)
+
+
+@pytest.mark.parametrize('seq', [
+    [66, 57, 42, 11, 52, 29, 29],
+    [9, 0, 7, 6, 4, 2],
+])
+def test_extract_max(seq):
+    h = MaxHeap(seq)
+    seq.sort(reverse=True)
+    for maxi in seq:
+        assert h.extract_max() == maxi
 
 
 @pytest.mark.timeout(3)
@@ -157,15 +169,15 @@ def stress():
     import random
     while True:
         n = random.randint(1, 1e4)
-        yield (random.randint(0, 1e9) for _ in range(n))
+        yield [random.randint(0, 1e9) for _ in range(n)]
 
 
-@pytest.mark.stress
-@pytest.mark.parametrize('iterable', islice(stress(), 4))
-def test_stress(iterable):
+@pytest.mark.skip
+@pytest.mark.parametrize('seq', islice(stress(), 10))
+def test_stress(seq):
     h1 = BuiltinMaxHeap()
     h2 = MaxHeap()
-    for x in iterable:
+    for x in seq:
         h1.insert(x)
         h2.insert(x)
     assert h1._list == h2._list
@@ -175,5 +187,4 @@ def test_stress(iterable):
         assert x1 == x2
 
 if __name__ == '__main__':
-    # main()
-    pytest.main([__file__])
+    main()
